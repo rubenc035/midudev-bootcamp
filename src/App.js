b44b94ef73381d06fe14374cdf89824914f2c882
rubenc035 <rubenc035@gmail.com>
2022-02-15 18:54:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; //IMPORTA LOS ESTILOS
+import Mensaje from './Mensaje.js';
 
-function App() {
+
+
+function App()  { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+			<Mensaje color='red' message='Estamos trabajando'/>
+      <Mensaje color='blue' message='en un curso'/>
+      <Mensaje color='green' message='de REACT'></Mensaje>
     </div>
   );
 }
